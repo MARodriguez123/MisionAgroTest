b82ee26dc1e3377d63a4e3ba81002001e8f0dace
Laura Pacheco <lcpachecop15@gmail.com>
2022-10-07 18:34:52 -0500

    Actualización con uso solo controller

--- src/main/java/mision/agro/mision_agro/service/impl/CatalogServiceImpl.java
+++ src/main/java/mision/agro/mision_agro/service/impl/CatalogServiceImpl.java
@@ -4,90 +4,71 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-<<<<<<< HEAD
-import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Sort;
-import org.springframework.stereotype.Service;
-
-import lombok.AllArgsConstructor;
-import mision.agro.mision_agro.controller.dto.CategoryDto;
-import mision.agro.mision_agro.controller.dto.ProductDto;
-=======
 import org.springframework.data.domain.Sort;
-import org.springframework.stereotype.Service;
 
-import lombok.AllArgsConstructor;
-<<<<<<< HEAD
 import mision.agro.mision_agro.controller.dto.CategoryDto;
 import mision.agro.mision_agro.controller.dto.ProductDto;
-=======
-import mision.agro.mision_agro.Controller.dto.CategoryDto;
-import mision.agro.mision_agro.Controller.dto.ProductDto;
->>>>>>> fdcdb041addb9f0baae2b67994a1b9dc801d9a12
->>>>>>> b2aa728136e0a61ebcb98a596bed263f6ce01c9e
 import mision.agro.mision_agro.model.Repository.CategoryRepository;
 import mision.agro.mision_agro.model.Repository.ProductRepository;
 import mision.agro.mision_agro.model.Repository.UserRepository;
 import mision.agro.mision_agro.service.CatalogService;
 
-@AllArgsConstructor
-@Service
-
-public class CatalogServiceImpl implements CatalogService{
+public class CatalogServiceImpl implements CatalogService {
 
     private final CategoryRepository categoryRepository;
     private final ProductRepository productRepository;
     private final UserRepository userRepository;
 
-
     @Override
     public List<CategoryDto> getCategories() {
-        var categories= categoryRepository.findAll(Sort.by("name"));
+        var categories = categoryRepository.findAll(Sort.by("name"));
 
         return categories.stream()
-        .map(cat -> new CategoryDto(cat.getNombre(), cat.getId().intValue()))
-        .collect(Collectors.toList()); 
-        
+                .map(cat -> new CategoryDto(cat.getNombre(), cat.getId().intValue()))
+                .collect(Collectors.toList());
+
     }
 
     @Override
     public Optional<CategoryDto> getCategoryById(Integer id) {
         var category = productRepository.deleteAllById(id.longValue());
-        if (category.isEmpty()){
+        if (category.isEmpty()) {
             return Optional.empty();
         }
-        
-        return Optional.of(new CategoryDto(
-        category.get().getNombre(),
-        category.get().getId().intValue())
-        );
 
-       
+        return Optional.of(new CategoryDto(
+                category.get().getNombre(),
+                category.get().getId().intValue()));
+
     }
 
     @Override
-    public List<ProductDto> getProductsByCategoryId(Integer categoryId) {
-        var product = productRepository.findAllByCategoryId(categoryId.longValue());
-
-        var categoryProducts= 
-        
-
-        
-    }
+        public List<ProductDto> getProductsByCategoryId(Integer categoryId) {
+            var products = productRepository.findAllByCategoryId(categoryId.longValue());
+    
+            var categoryProducts= product.stream()
+            .map(pro-> ProductDto.builder()
+                    .id(pro.getCode())
+                    .nombre(pro.getNombre())
+                    ,
+    
+    
+    
+            );
+            
+            return categoryProducts;
+        }
 
     @Override
     public ProductDto getProductsById(String id) {
-        
-        
 
         return null;
     }
 
     @Override
     public List<ProductImageDto> getProductsImageById(String id) {
-        
-        
 
         return null;
     }
-    
-}
+
+}}
