824bf6a8ba14858f3467433826b4234d75938206
cejoven-deb <carlosvivax77@hotmail.com>
2022-10-07 22:33:03 -0500

    Todos los cambios hechos

--- src/main/java/mision/agro/mision_agro/service/impl/CatalogServiceImpl.java
+++ src/main/java/mision/agro/mision_agro/service/impl/CatalogServiceImpl.java
@@ -1,6 +1,20 @@
 package mision.agro.mision_agro.service.impl;
 
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import lombok.AllArgsConstructor;
+import mision.agro.mision_agro.controller.dto.CategoryDto;
 import mision.agro.mision_agro.controller.dto.ProductDto;
+import mision.agro.mision_agro.model.Repository.CategoryRepository;
+import mision.agro.mision_agro.model.Repository.ProductRepository;
+import mision.agro.mision_agro.model.Repository.UserRepository;
+import mision.agro.mision_agro.service.CatalogService;
 
 @AllArgsConstructor
 @Service
@@ -65,9 +79,20 @@ public class CatalogServiceImpl implements CatalogService {
                  .descripcion(product.getDescripcion())
                  .userId(product.getUserId())
                  .imagenUrl(product..getImagenUrl())
-                 .build()
+                 .build();
+
+    }
+
+    @Override
+    public List<ProductDto> getMoviesByFilter(FilterDto filter) {
+        var tipos= filter.getTipo() != null;
+        var variedad = filter.getVariedad() != null;
+
+        if(tipos && variedad) {
+            // movieRepository.findAllByTypeIdInAndSchoolIdIn(filter.getType(), filter.getSchool());
+            // SELECT * FROM movie WHERE type in ('d') and school in ('1', '2');
+        }
+        return null;
     }
 
 }
-
-
