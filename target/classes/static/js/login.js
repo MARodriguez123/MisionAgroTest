1129abb9b96864045e50652b1d3c2101bec82b48
Laura Pacheco <lcpachecop15@gmail.com>
2022-10-07 20:05:31 -0500

    Actualización de login en JSON

--- target/classes/static/js/login.js
+++ target/classes/static/js/login.js
@@ -1,62 +1,66 @@
-let login =() =>{
-  let email = document.getElementById("floatingInput").value;
+const login = () => {
+    const email = document.getElementById("floatingInput").value;
 
-  let password =document.getElementById("floatingPassword").value;
+    const password = document.getElementById("floatingPassword").value;
 
-  if( email == ""){
-   showError("El correo es requerido");
-   return;
+    if (email == "") {
+        showError("El correo es requerido");
+        return;
 
 
-  }
-if (password == ""){
+    }
+    if (password == "") {
 
-    showError("La Contraseña es requerida");
-    return;
-}
+        showError("La Contraseña es requerida");
+        return;
+    }
 
-postToLogin(body);
+    const body = {
+        "correo": email,
+        "contrasela": password,
+    };
+    postToLogin(body);
 
 
 };
 
-let postToLogin = async(bodyObject) =>{
-    let url = "/api/login";
-    let response = await fetch(url, {
-        method :"POST",
-        body:JSON.stringgify(bodyObject),
-        headers:{
+const postToLogin = async (bodyObject) => {
+    const url = "/api/login";
+    const response = await fetch(url, {
+        method: "POST",
+        body: JSON.stringgify(bodyObject),
+        headers: {
 
-            "Content-Type":"application/json"
+            "Content-Type": "application/json"
 
         }
     });
 
-    if(response.ok){
-        let  user = await response.json();
-        localStorage.setItem("loggedEmail", JSON.stringify(email) );
-        alert("Te damos la Bienvenida" + user.email+ "!", "success");
+    if (response.ok) {
+        const user = await response.json();
+        localStorage.setItem("loggedEmail", JSON.stringify(user));
+        alert("Te damos la Bienvenida" + user.email + "!", "success");
 
-        await new Promise(r=>setTimeout(r,2000));
-        window.location.href="/publicaciones"
+        await new Promise(r => setTimeout(r, 2000));
+        window.location.href = "/publicaciones"
 
-        }else{
-            let message = await response.text();
-            showError(message);
-        }
+    } else {
+        const message = await response.text();
+        showError(message);
+    }
 };
 
-let showError = (message)=>{
-    alert(message,"danger");
+const showError = (message) => {
+    alert(message, "danger");
 
 }
 
-let alert =(message,type)=>{
-   document.getElementById("errorBox").innerHTML=[
-    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
-    `   <div>${message}</div>`,
-    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
-    '</div>'
-   ].join('')
+let alert = (message, type) => {
+    document.getElementById("errorBox").innerHTML = [
+        `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+        `   <div>${message}</div>`,
+        '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+        '</div>'
+    ].join('')
 }
 
